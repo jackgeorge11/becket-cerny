0cbd5c47be748783daec16e06aeccaa6816e7880
jackgeorge11 <jackkgeorgee@gmail.com>
2021-12-11 13:48:27 +0000

    began styling and built out structure of site

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function MenuComponent() {
+    return (
+        <div menu-component>
+            
+        </div>
+    )
+}
+
+export default MenuComponent
